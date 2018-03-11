bf496126ccf58830ea6c6bde6e59e4e12c24adc7
Filipe Medeiros <fcma@cin.ufpe.br>
2018-03-11 19:44:24 -0300

    Sistema de templates

--- core/views.py
+++ core/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-	return HttpResponse('Hello World!')
+	texts = ['lorem ipsum dolor sit amet', 'consectetur adipisicing elit']
+	context = {
+		'title' : 'django e-commerce',
+		'texts' : texts,
+	}
+	return render(request, 'index.html', context)
