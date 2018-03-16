457bda2df94ff2728964a77765093b7c69b37c38
Filipe Medeiros <fcma@cin.ufpe.br>
2018-03-16 16:22:46 -0300

    Customizando o django admin

--- catalog/models.py
+++ catalog/models.py
@@ -14,6 +14,9 @@ class Category (models.Model):
 		verbose_name_plural = 'Categorias'
 		ordering = ['name']
 
+	def __str__(self):
+		return self.name
+
 class Product (models.Model):
 
 	name = models.CharField ('Nome', max_length=100)
@@ -27,7 +30,9 @@ class Product (models.Model):
 	modified = models.DateTimeField ('Modificado em', auto_now=True)
 
 	class Meta:
-		verbose_name = 'Categoria'
-		verbose_name_plural = 'Categorias'
+		verbose_name = 'Produto'
+		verbose_name_plural = 'Produtos'
 		ordering = ['name']
 
+	def __str__(self):
+		return self.name
