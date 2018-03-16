602328519c0a5cb7d848ca05845bf5503f4e1b87
Filipe Medeiros <fcma@cin.ufpe.br>
2018-03-16 11:18:02 -0300

    Teste no Heroku

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'k+6-0n-wa)4sm#vd)y7%sslzn6$xt$u)87d_8b6$mu9w-5_s*k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
