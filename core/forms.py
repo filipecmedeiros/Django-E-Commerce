72abc02c98abcd2f0998ddb0c0deee35341fcbc4
Filipe Medeiros <fcma@cin.ufpe.br>
2018-03-23 15:48:56 -0300

    Enviando email de contato

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 class ContactForm (forms.Form):
 
 	name = forms.CharField(label='Nome')
@@ -11,3 +14,13 @@ class ContactForm (forms.Form):
 	# 	self.fields['name'].widget.attrs['class'] = 'form-control'
 	# 	self.fields['email'].widget.attrs['class'] = 'form-control'
 	# 	self.fields['message'].widget.attrs['class'] = 'form-control'
+
+	def send_mail(self):
+		name = self.cleaned_data['name']
+		email = self.cleaned_data['email']
+		message = self.cleaned_data['message']
+		message = 'Nome: {0}\nEmail:{1}\n{2}'.format(name, email,message)
+		send_mail('Contato do Django E-Commerce', message, settings.DEFAULT_FROM_EMAIL,
+			[settings.DEFAULT_FROM_EMAIL]
+		)
+		return True
--- core/views.py
+++ core/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
 from .forms import ContactForm
 # Create your views here.
 def index (request):
 	return render(request, 'index.html')
 
 def contact (request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-	else:
-		form = ContactForm()
+	success = False
+	form = ContactForm(request.POST or None)
+	if form.is_valid():
+		success = form.send_mail()
 
 	context = {
 		'form':form,
+		'success':success,
 	}
 	return render (request, 'contact.html', context)
